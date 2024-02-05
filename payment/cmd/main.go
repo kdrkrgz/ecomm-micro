f7dabd5340d9239b6ced5a3573fd5009c13519b9
kadir karagöz <karagozkadir@hotmail.com>
2024-02-05 23:06:57 +0300

    main package added to payment service

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"context"
+	"github.com/kdrkrgz/ecomm-micro/payment/config"
+	"github.com/kdrkrgz/ecomm-micro/payment/internal/adapters/db"
+	"github.com/kdrkrgz/ecomm-micro/payment/internal/adapters/grpc"
+	"github.com/kdrkrgz/ecomm-micro/payment/internal/application/core/api"
 	log "github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.22.0"
 	"go.opentelemetry.io/otel/trace"
 	"os"
+)
+
+const (
+	service     = "payment"
+	environment = "dev"
+	id          = 1
 )
 
 func init() {
@@ -32,6 +50,39 @@ func (c customLogger) Format(entry *log.Entry) ([]byte, error) {
 	return c.formatter.Format(entry)
 }
 
-func main() {
+func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	ctx := context.Background()
+	exp, err := otlptracehttp.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in a Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(service),
+			attribute.String("environment", environment),
+			attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
+}
 
+func main() {
+	tp, err := tracerProvider("http://jaeger-otel.jaeger.svc.cluster.local:14278/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}))
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceUrl())
+	if err != nil {
+		log.Fatalf("Failed to connect database. Error: %v", err)
+	}
+	application := api.NewApplication(dbAdapter)
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
 }
--- payment/go.mod
+++ payment/go.mod
@@ -4,8 +4,13 @@ go 1.21.5
 
 require (
 	github.com/kdrkrgz/ecomm-proto/golang/payment v0.0.2
+	github.com/sirupsen/logrus v1.9.2
 	github.com/uptrace/opentelemetry-go-extra/otelgorm v0.2.3
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.47.0
+	go.opentelemetry.io/otel v1.22.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.22.0
+	go.opentelemetry.io/otel/sdk v1.22.0
+	go.opentelemetry.io/otel/trace v1.22.0
 	google.golang.org/grpc v1.61.0
 	gorm.io/driver/postgres v1.5.5
 	gorm.io/gorm v1.25.6
@@ -13,23 +18,25 @@ require (
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/sirupsen/logrus v1.9.2 // indirect
 	github.com/uptrace/opentelemetry-go-extra/otelsql v0.2.3 // indirect
-	go.opentelemetry.io/otel v1.22.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.22.0 // indirect
 	go.opentelemetry.io/otel/metric v1.22.0 // indirect
-	go.opentelemetry.io/otel/trace v1.22.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )

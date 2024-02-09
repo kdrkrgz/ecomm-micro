4b0d4686e4305d70260776c16b966b868add41fd
kadir karagöz <karagozkadir@hotmail.com>
2024-02-09 20:43:22 +0300

    ingress added and some config

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,2 +1,2 @@
-psql -U ecommdb_root -tc "SELECT 1 FROM pg_database WHERE datname = 'orders'" | grep -q 1 || psql -U ecommdb_root -c "CREATE DATABASE orders"
-psql -U ecommdb_root -tc "SELECT 1 FROM pg_database WHERE datname = 'payments'" | grep -q 1 || psql -U ecommdb_root -c "CREATE DATABASE payments"
+CREATE DATABASE orders;
+CREATE DATABASE payments:
